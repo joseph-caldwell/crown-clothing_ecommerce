86185de5351afa16fab20dc425ed4edf08c5f89b
Joseph Caldwell <caldwelljoey36@gmail.com>
2022-11-29 23:44:42 -0600

    adding navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from "./routes/home/home.component.jsx";
+import Navigation from './routes/navigation/navigation.component.jsx';
+
+
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={ <Navigation /> } >
+        <Route index element={ <Home /> } />
+        <Route path='shop' element={ <Shop /> } />
+      </Route>
     </Routes>
+    
   );
  }
 
